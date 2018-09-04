b11ff03847ad456f1f83a750ebf1a64a616ff736
thanh4890 <thanh4890@gmail.com>
2018-09-04 15:30:02 +0700

    Simulate api server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
 import { Observable, of } from 'rxjs'
 import { MessageService } from './message.service'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private heroesUrl = 'api/heroes'
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes')
-    return of(HEROES)
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`)
-    return of(HEROES.find(hero => hero.id === id))
+    const url = `${this.heroesUrl}/${id}`
+    return this.http.get<Hero>(url)
   }
 }
